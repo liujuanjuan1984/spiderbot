e172ca40578412a4c353ec8e987c64a729da8270
liujuanjuan1984 <liujuanjuan1984@gmail.com>
2022-09-27 12:15:22 +0800

    [+]examples add keyboardinterrupt

--- examples/b_get_contents.py
+++ examples/b_get_contents.py
@@ -1,6 +1,20 @@
+import datetime
+import signal
+import sys
+
 from spiderbot import SpiderBot
 
 bot = SpiderBot()
+
+
+def sigint_handler(signal, frame):
+    print(datetime.datetime.now(), "SIGINT received. Exiting...")
+    bot.quit()
+    print(datetime.datetime.now(), "SpiderBot quit.")
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, sigint_handler)
 
 bot.get_profiles()
 bot.get_new_posturls()
--- examples/c_forever.py
+++ examples/c_forever.py
@@ -1,12 +1,17 @@
+import datetime
 import time
 
 from spiderbot import SpiderBot
 
 bot = SpiderBot()
 
+
 while True:
-    bot.get_new_posturls()
-    bot.get_posts()
-    time.sleep(1)
+    try:
+        bot.get_new_posturls()
+        bot.get_posts()
+    except KeyboardInterrupt:
+        print(datetime.datetime.now(), "KeyboardInterrupt received. Exiting...")
+        break
 
 bot.quit()
